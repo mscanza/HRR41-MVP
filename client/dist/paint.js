cd0a4fae0a18f3b1569f1726d9b8654c74aba5ee
Michael Scanza <mscanza@gmail.com>
2019-11-16 22:52:48 -0800

    Setup basic paint

---
+++ client/dist/paint.js
@@ -0,0 +1,149 @@
+$( document ).ready(function() {
+  console.log( "ready!" );
+
+let colorsArray = ['black', 'white', 'red', 'blue', 'lime', 'cyan', 'magenta','silver', 'gray','maroon','olive','green','purple','teal','navy', 'aliceblue', 'dodgerblue', 'bisque', 'blueviolet','aquamarine', 'chartreuse','chocolate','coral','cornflowerblue','crimson', 'darkorange','darkseagreen','deeppink', 'dimgray','gold','goldenrod','greenyellow','indigo','ivory','khaki']
+let colorTimer = 25;
+let strokeList = [];
+let colorPosition = 0;
+let brushColor = 'black';
+let brushStatus = document.getElementById('brushStatus');
+let body = document.getElementsByTagName('body')[0];
+let square = document.getElementById('square');
+let circle = document.getElementById('circle');
+let colors = document.getElementById('colors');
+let swatch = document.getElementById('swatch');
+let plus = document.getElementById("plus");
+let minus = document.getElementById("minus");
+let canvas = document.getElementById("canvas");
+
+
+for (let i = 0; i < colorsArray.length; i++) {
+let div = document.createElement('div');
+div.classList.add('eachColor');
+div.style.left = colorPosition + 'px';
+colorPosition += 20;
+div.style.backgroundColor = colorsArray[i];
+window.setTimeout(function(){
+  colors.append(div)
+}, colorTimer)
+colorTimer += 50;
+}
+
+
+brushStatus.style.opacity = '0.2';
+
+
+let swatchWidth = 10;
+let swatchHeight = 10;
+
+swatch.style.width = swatchWidth + 'px';
+swatch.style.height = swatchHeight + 'px';
+swatch.style.borderRadius = '50%';
+swatch.style.backgroundColor = brushColor;
+
+square.addEventListener('click', function(){
+square.style.border = '2px solid mediumSlateBlue';
+square.style.top = '31px';
+circle.style.border = 'none';
+circle.style.top = '33px';
+swatch.style.borderRadius = '0px';
+})
+
+circle.addEventListener('click', function(){
+circle.style.border = '2px solid mediumSlateBlue';
+circle.style.top = '31px';
+square.style.border = 'none';
+square.style.top = '33px';
+swatch.style.borderRadius = '50%';
+})
+
+plus.addEventListener('click',function(){
+if (swatchWidth <= 20) {
+    swatchWidth += 2;
+swatchHeight += 2;
+swatch.style.width = swatchWidth + 'px';
+swatch.style.height = swatchHeight + 'px';
+}
+
+})
+minus.addEventListener('click',function(){
+if (swatchWidth > 2) {
+    swatchWidth -= 2;
+swatchHeight -= 2;
+swatch.style.width = swatchWidth + 'px';
+swatch.style.height = swatchHeight + 'px';
+}
+
+})
+
+
+
+let toggler = false;
+
+let myListener = function(event) {
+if (event.clientY <= 100) {
+  return;
+}
+brushStatus.style.opacity = '1'
+let stroke = document.createElement('div');
+stroke.classList.add('stroke');
+stroke.style.width = swatchWidth + 'px';
+stroke.style.height = swatchHeight + 'px';
+stroke.style.backgroundColor = brushColor;
+stroke.style.position = 'absolute';
+stroke.style.borderRadius = swatch.style.borderRadius;
+stroke.style.left = event.clientX + 'px';
+stroke.style.top = event.clientY + 'px';
+    if (event.clientY <= 100) {
+      stroke.style.display = 'none';
+    }
+canvas.append(stroke)
+  }
+document.addEventListener('click', function(e){
+if (e.target.classList.value.includes('eachColor')) {
+
+ for (let i = 0; i < colorsArray.length; i++) {
+  let item =  document.getElementsByClassName('eachColor')[i];
+   item.style.border = 'none';
+   item.style.top = '0px';
+   item.style.width = '20px';
+   item.style.zIndex = '0';
+ }
+
+ e.target.style.border = '2px solid mediumSlateBlue';
+ e.target.style.top = '-2px';
+ e.target.style.width = '18px';
+ e.target.style.zIndex = '1';
+ brushColor = e.target.style.backgroundColor;
+ swatch.style.backgroundColor = brushColor;
+}
+if (toggler) {
+  brushStatus.style.opacity = '0.2'
+  document.removeEventListener('mousemove', myListener)
+  toggler = false;
+  return;
+} else if (event.clientY <= 100){
+  return;
+} else {
+toggler = true;
+    document.addEventListener('mousemove', myListener)
+
+}
+})
+
+function savePainting() {
+
+strokeList = $('#canvas').children();
+
+}
+function appendStrokes() {
+console.log(strokeList)
+$('#canvas').html('');
+for (let i = 1; i < strokeList.length; i++) {
+$('#canvas').append(strokeList[i]);
+}
+}
+document.getElementById('back').addEventListener('click', appendStrokes);
+document.getElementById('save').addEventListener('click', savePainting);
+
+});
--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,59 @@
 import React, { Component } from "react";
-// import "./App.css";
+import "../css/App.css";
+import $ from "jquery";
 
 class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Michael',
+      paintings: ['My painting1', 'Painting 2'],
+      selected: 'new'
+    }
+  }
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! </h1>
+        <div className="welcome">
+          <h1>Welcome, {this.state.name}</h1>
+          <span>Your paintings:</span>
+          <select onChange={(e) => {this.setState({selected: e.target.value})}}id="paintings">
+            <option value="new">New</option>
+          {this.state.paintings.map((item) => {
+            return (
+            <option value={item}>{item}</option>
+            )
+          })}
+          </select>
+          {this.state.selected === 'new' ? (
+            <span>
+              <input type="text" placeholder="Enter Painting Name" />
+              <button>Save New Painting</button>
+            </span>
+          ) : (
+            <span>
+              <button>Save</button>
+              <button>Delete</button>
+            </span>
+          )}
+        </div>
+        <div id="sizer">
+          <span id="brushStatus">BRUSHING</span>
+          <div id="circle"></div>
+          <div id="square"></div>
+          <div id='sizing-container'>
+            <button className="size-button" id="minus">-</button>
+            <div id="swatch"></div>
+            <button className="size-button" id="plus">+</button>
+          </div>
+          <div id="colors"></div>
+          <button id="save">Save</button>
+          <button id="back">bring back</button>
+        </div>
+        <div id="canvas">
+          <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
+          <span id="instructions">Click anywhere on canvas to activate brush.</span>
+        </div>
       </div>
     );
   }
